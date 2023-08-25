52abf64f9d53af7ab3b22ddfa83ab3f773b2c05f
nicollyADS <rm552579@fiap.com.br>
2023-08-24 22:58:32 -0300

    criando modularização com componentes

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,8 @@
+import ReactLogo from "./assets/react.svg"
+import Cabecalho from "./components/cabecalho"
+
+let reactLogoTextoAlt = "Logo do React"
+
 export default function App() {
 
   //AREA DECLARATIVA
@@ -6,10 +11,29 @@ export default function App() {
   return (
     <>
       {/* AREA IMPERATIVA */}
+  
       <div>
-        <h1>Vite+React</h1>
-        <h2>Conseguimos!</h2>
-        <p>Olá Mundo!</p>
+        <Cabecalho></Cabecalho>
+        <session>
+          <div>
+            <p>sdsdsd</p>
+            <p>jdskdjs</p>
+            <p>dsdshd</p>
+            <img src={ReactLogo} alt={reactLogoTextoAlt} />
+          </div>
+        </session>
+
+        <footer>
+          <div>
+            <ul>
+              <li><a href="#"></a></li>
+              <li><a href="#"></a></li>
+              <li><a href="#"></a></li>
+            </ul>
+          </div>
+        </footer>
+
+
       </div>
     </>
   )
---
+++ src/components/Cabecalho.jsx
@@ -0,0 +1,16 @@
+export default function Cabecalho() {
+
+    return(
+        <>
+        <header>
+          <h1>Vite + React / Coded By - RM552579</h1>
+          <ul>
+            <li><a href="#">Item 1</a></li>
+            <li><a href="#">Item 2</a></li>
+            <li><a href="#">Item 3</a></li>
+          </ul>
+        </header>
+        </>
+    )
+
+}
