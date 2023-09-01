8f2b34476ab2350adcb7b0647304addabee92017
nicollyADS <rm552579@fiap.com.br>
2023-08-31 22:15:33 -0300

    criação do modulo de listaObjetos

---
+++ src/components/ListaProdutos.jsx
@@ -0,0 +1,10 @@
+export const ListaProdutos = [
+    {id: 1, nome: "Teclado", desc: "Teclado 102 Teclas - Mecânico.", preco: 200},
+    {id: 2, nome: "Mouse", desc: "Mouse Gamer - 3200 DPI.", preco: 100},
+    {id: 3, nome: "Monitor", desc: "Monitor 24' - 144Hz.", preco: 800},
+    {id: 4, nome: "Headset", desc: "Headset Gamer - 7.1 Surround.", preco: 300},
+    {id: 5, nome: "Mousepad", desc: "Mousepad Gamer - 80x30cm.", preco: 50},
+    {id: 6, nome: "Cadeira", desc: "Cadeira Gamer - 180°.", preco: 1000},
+    {id: 7, nome: "Gabinete", desc: "Gabinete Gamer - Vidro Temperado.", preco: 500},
+    {id: 8, nome: "Placa de Vídeo", desc: "Placa de Vídeo - 8GB.", preco: 2000}
+]
