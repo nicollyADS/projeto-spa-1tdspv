55ccbe3a0bd0b64ca7385127b213722d2164cac0
nicollyADS <rm552579@fiap.com.br>
2023-09-12 23:01:27 -0300

    use effect

--- src/routes/Produtos.jsx
+++ src/routes/Produtos.jsx
@@ -2,14 +2,25 @@ import { ListaProdutos } from "../components/ListaProdutos";
 import { Link } from "react-router-dom";
 import {AiFillEdit as Editar, AiOutlineDelete as Excluir} from "react-icons/ai";
 import classes from "./Produtos.module.css";
+import { useEffect, useState } from "react";
 
 export default function Produtos() {
 
     document.title = "Lista de Produtos";
 
+    useEffect(()=>{
+      console.log("use effect que sera sempre renderizado!")
+    });
+
+    const [exemplo, setExemplo] = useState(0)
+
     return (
       <div>
           <h1>LISTA DE PRODUTOS</h1>
+
+          <div>
+            <button onClick={()=> setExemplo(exemplo, 1)} >CLICK - {exemplo}</button>
+          </div>
 
         <div>
           <table className={classes.tableStyle} >
