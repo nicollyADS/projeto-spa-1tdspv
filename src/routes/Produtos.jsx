93c335c7ed6a7af8a44c23b96ce1680a1a80236f
nicollyADS <rm552579@fiap.com.br>
2023-09-12 22:05:42 -0300

    CSS.MODULE

--- src/routes/Produtos.jsx
+++ src/routes/Produtos.jsx
@@ -1,6 +1,7 @@
 import { ListaProdutos } from "../components/ListaProdutos";
 import { Link } from "react-router-dom";
 import {AiFillEdit as Editar, AiOutlineDelete as Excluir} from "react-icons/ai";
+import classes from "./Produtos.module.css";
 
 export default function Produtos() {
 
@@ -11,24 +12,24 @@ export default function Produtos() {
           <h1>LISTA DE PRODUTOS</h1>
 
         <div>
-          <table>
+          <table className={classes.tableStyle} >
             <thead>
-              <tr>
-                <th>ID</th>
-                <th>Nome</th>
-                <th>Descrição</th>
-                <th>Preço</th>
-                <th>Editar/Excluir</th>
+              <tr className={classes.tableHeaderStyle}>
+                <th className={classes.tableHeaderStyle}>ID</th>
+                <th className={classes.tableHeaderStyle}>Nome</th>
+                <th className={classes.tableHeaderStyle}>Descrição</th>
+                <th className={classes.tableHeaderStyle}>Preço</th>
+                <th className={classes.tableHeaderStyle}>Editar/Excluir</th>
                 </tr>
             </thead>
             <tbody>
               {ListaProdutos.map((produto, index) => (
-                <tr key={index}>
-                  <td>{produto.id}</td>
-                  <td>{produto.nome}</td>
-                  <td>{produto.desc}</td>
-                  <td>{produto.preco}</td>
-                  <td><Link to={`/editar/produtos/${produto.id}`}><Editar/></Link> | <Link to={`/excluir/produtos/${produto.id}`}><Excluir/></Link></td>
+                <tr key={index} className={classes.tableLineStyle}>
+                  <td className={classes.tableDataStyle}>{produto.id}</td>
+                  <td className={classes.tableDataStyle}>{produto.nome}</td>
+                  <td className={classes.tableDataStyle}>{produto.desc}</td>
+                  <td className={classes.tableDataStyle}>{produto.preco}</td>
+                  <td className={classes.tableDataStyle}><Link to={`/editar/produtos/${produto.id}`}><Editar/></Link> | <Link to={`/excluir/produtos/${produto.id}`}><Excluir/></Link></td>
                 </tr>
               ))}
             </tbody>
