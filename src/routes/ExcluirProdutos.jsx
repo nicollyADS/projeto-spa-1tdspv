812b7c1ed2557b196fc756dab4b4cb154e1c7ca5
nicollyADS <rm552579@fiap.com.br>
2023-09-14 23:13:59 -0300

    estrutura de excluir produtos

--- src/routes/ExcluirProdutos.jsx
+++ src/routes/ExcluirProdutos.jsx
@@ -1,7 +1,21 @@
-import React from 'react'
-
 export default function ExcluirProdutos() {
   return (
-    <div>ExcluirProdutos</div>
+    <>
+      <h1>Excluir Produtos</h1>
+      <div>
+          <section>
+              <h2>PRODUTO SELECIONADO PARA EXCLUSÃO</h2>
+              <h3>Você tem certeza de que deseja excluir esse produto?</h3>
+              <figure>
+                <img src="" alt="" />
+                <figcaption>
+                  Nome - R$ Preço
+                </figcaption>
+              </figure>
+              <button>Excluir</button>
+              <button>Cancelar</button>  
+          </section>
+      </div>
+    </>
   )
 }
