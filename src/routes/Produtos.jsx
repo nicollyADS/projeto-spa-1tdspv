ce942a6d6526600f0a146fec74e3e2e6374bf7ae
nicollyADS <rm552579@fip.com.br>
2023-09-19 23:04:50 -0300

    exemplo de json-server com listagem dinamica

--- src/routes/Produtos.jsx
+++ src/routes/Produtos.jsx
@@ -8,16 +8,25 @@ export default function Produtos() {
 
     document.title = "Lista de Produtos";
 
-   
+    const [listaProdutoLocal, setListaProdutoLocal] = useState([{}])
+    useEffect(()=>{
+
+      fetch('http://localhost:5000/produtos/',{
+        method: 'GET',
+        headers:{
+          'Content-Type': 'application/json',
+        },
+      }).then((response)=> response.json())
+      .then((data)=>{
+        setListaProdutoLocal(data);
+      })
+      .catch((err)=>console.log(err));
+
+    },[]);
 
     return (
       <div>
           <h1>LISTA DE PRODUTOS</h1>
-
-
-        <div>
-          <button onClick={()=> setCount(count + 1)}>COUNTER - {count}</button>
-        </div>
 
         <div>
           <table className={classes.tableStyle}>
@@ -32,7 +41,7 @@ export default function Produtos() {
                 </tr>
             </thead>
             <tbody>
-              {exemplo.map((produto, index) => (
+              {listaProdutoLocal.map((produto, index) => (
                 <tr key={index} className={classes.tableLineStyle}>
                   <td className={classes.tableDataStyle}>{produto.id}</td>
                   <td className={classes.tableDataStyle}>{produto.nome}</td>
@@ -73,3 +82,8 @@ export default function Produtos() {
   // const [exemplo, setExemplo] = useState([{}]);
 
   // const [count, setCount] = useState(0);
+
+
+//   <div>
+//   <button onClick={()=> setCount(count + 1)}>COUNTER - {count}</button>
+// </div>
