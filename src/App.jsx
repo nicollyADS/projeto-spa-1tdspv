1487e732161a596ab6162afbc331aa97a617f72e
nicollyADS <rm552579@fiap.com.br>
2023-10-03 23:58:08 -0300

    estilizando o rodape com scss

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import Cabecalho from "./components/Cabecalho/Cabecalho";
-import Rodape from "./components/Rodape";
+import Rodape from "./components/Rodape/Rodape";
 import Conteudo from "./components/Conteudo"
 import { Outlet } from "react-router-dom";
 
--- src/components/Rodape/Rodape.jsx
+++ src/components/Rodape/Rodape.jsx
@@ -1,16 +1,19 @@
+import "./Rodape.scss"
+
+
 export default function Rodape() {
   return (
     <>
-      <footer>
+      <footer className="footer">
         <div>
-          <ul>
-            <li>
+          <ul className="list-items">
+            <li className="item">
               <a href="#">GitHub</a>
             </li>
-            <li>
+            <li className="item">
               <a href="#">X</a>
             </li>
-            <li>
+            <li className="item">
               <a href="#">Reddit</a>
             </li>
           </ul>
